4ea11e057af3831cd816c0573bbd73979791d9f5
delvedor <tommydelved@gmail.com>
2017-07-21 22:04:54 +0200

    Updated slides

--- presentation/index.js
+++ presentation/index.js
@@ -9,15 +9,19 @@ import {
   Slide,
   Image,
   Text,
-  List,
-  ListItem,
-  Appear
+  Appear,
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderItem,
+  TableRow,
+  TableItem
 } from 'spectacle'
 
 // Import code style formatter
 import CodeSlide from 'spectacle-code-slide'
 // Import Terminal formatter
-import Terminal from 'spectacle-terminal'
+// import Terminal from 'spectacle-terminal'
 
 // Import image preloader util
 import preloader from 'spectacle/lib/utils/preloader'
@@ -33,12 +37,20 @@ const images = {
   letzdoit: require('../assets/letzdoit.png'),
   nodeExpress: require('../assets/node-express.png'),
   zerox: require('../assets/0x.png'),
+  zeroxDemo: require('../assets/0x-demo.gif'),
   autocannon: require('../assets/autocannon.png'),
+  autocannonDemo: require('../assets/autocannon-demo.gif'),
   flamegraphExpress: require('../assets/flamegraph-express.png'),
   flamegraphNode: require('../assets/flamegraph-node.png'),
   fastifyWhiteHorizontal: require('../assets/fastify-white-landscape.png'),
   fastifyWhiteVertical: require('../assets/fastify-white-portrait.png'),
   nodeExpressFastify: require('../assets/node-expess-fastify.png'),
+  rocket: require('../assets/rocket.jpg'),
+  speed: require('../assets/speed.jpg'),
+  buildRun: require('../assets/build-run.png'),
+  router: require('../assets/router.png'),
+  json: require('../assets/json.png'),
+  helmet: require('../assets/helmet.png'),
   eventLoop: require('../assets/event-loop.png')
 }
 
@@ -54,11 +66,13 @@ const theme = createTheme({
   secondary: 'Helvetica'
 })
 
+const tablePadding = '40px 0 0 0'
+
 export default class Presentation extends React.Component {
   render () {
     return (
       <Deck transition={['slide']} transitionDuration={500} theme={theme}>
-        <Slide transition={['slide']} bgColor='tertiary'>
+        <Slide transition={['slide']} bgImage={images.rocket}>
           <Heading size={1} caps textColor='primary'>{'What if I told you that HTTP can be fast?'}</Heading>
           <Text size={6} margin={'150px 0 0 0'}>
             <Link href='https://github.com/delvedor' target='_blank' textColor='secondary'>@delvedor</Link>
@@ -93,7 +107,7 @@ export default class Presentation extends React.Component {
           <Image src={images.nodeExpress} width={750} />
         </Slide>
 
-        <Slide transition={['slide']} bgColor='tertiary'>
+        <Slide transition={['slide']} bgColor='secondary'>
           <Heading caps textColor='primary'>{'Let me introduce you some tooling'}</Heading>
         </Slide>
 
@@ -102,10 +116,18 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Image src={images.zeroxDemo} width={1400} />
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
           <Image src={images.autocannon} width={1300} />
         </Slide>
 
-        <Slide transition={['slide']} bgColor='tertiary'>
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Image src={images.autocannonDemo} width={1000} />
+        </Slide>
+
+        {/* <Slide transition={['slide']} bgColor='tertiary'>
           <Heading textColor='primary' size={3} margin={'0 0 50px 0'}>{'Run 0x'}</Heading>
           <Terminal title='1. tmux -2 attach -t 0 (tmux)' output={[
             '0x benchmarks/express.js',
@@ -142,7 +164,7 @@ export default class Presentation extends React.Component {
             </div>
           ]}
           />
-        </Slide>
+        </Slide> */}
 
         <Slide transition={['slide']} bgColor='quartenary' align='center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
           <Image src={images.flamegraphExpress} width={window.innerWidth - 100} />
@@ -156,29 +178,175 @@ export default class Presentation extends React.Component {
           <Image src={images.fastifyWhiteVertical} height={window.innerHeight - 200} />
         </Slide>
 
-        <Slide transition={['slide']} bgColor='tertiary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
-          <Heading textColor='primary' size={5}>Fastify features</Heading>
-          <List>
-            <Appear><ListItem>Rich plugin system</ListItem></Appear>
-            <Appear><ListItem>Middleware support</ListItem></Appear>
-            <Appear><ListItem>Lifecycle methods</ListItem></Appear>
-            <Appear><ListItem>Decorators</ListItem></Appear>
-            <Appear><ListItem>Validations</ListItem></Appear>
-            <Appear><ListItem>Logging</ListItem></Appear>
-            <Appear><ListItem>Performances</ListItem></Appear>
-          </List>
-        </Slide>
+        <CodeSlide
+          transition={['slide']}
+          lang='js'
+          code={require('raw-loader!../assets/code.example')}
+          ranges={[
+            { loc: [0, 10], title: 'Fastify Server' }
+          ]}
+        />
 
         <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
           <Image src={images.nodeExpressFastify} width={900} />
         </Slide>
 
-        <Slide transition={['slide']} bgColor='tertiary'>
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHeaderItem textSize={40}>Feature</TableHeaderItem>
+                <TableHeaderItem textSize={40}>Express</TableHeaderItem>
+                <TableHeaderItem textSize={40}>Hapi</TableHeaderItem>
+                <TableHeaderItem textSize={40}>Fastify</TableHeaderItem>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Routing</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Middleware</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Plugins</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Decorators</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Hooks</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Validation</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>Logging</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>❌</TableItem>
+                <TableItem textSize={40} padding={tablePadding}>✅</TableItem>
+              </TableRow>
+              <TableRow>
+                <TableItem textSize={40} padding={tablePadding}>req/sec</TableItem>
+                <TableItem textSize={40} textColor='red' padding={tablePadding}>8534</TableItem>
+                <TableItem textSize={40} textColor='red' padding={tablePadding}>2200</TableItem>
+                <TableItem textSize={40} textColor='green' padding={tablePadding}>21682</TableItem>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Slide>
+
+        <Slide transition={['slide']} bgImage={images.speed} bgDarken={0.6}>
           <Heading caps textColor='primary'>{'How can Fastify be so fast?'}</Heading>
         </Slide>
 
         <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
-          <Image src={images.eventLoop} width={window.innerWidth - 200} />
+          <Image src={images.buildRun} width={window.innerWidth - 450} />
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Heading size={6} textColor='secondary'>{'Serialization'}</Heading>
+          <Text textSize={40}><Link href='https://github.com/fastify/fast-json-stringify'>fast-json-stringify</Link></Text>
+          <Image src={images.json} fit />
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Heading size={6} textColor='secondary'>{'Why not JSON.stringify?'}</Heading>
+          <Appear><Text textSize={40} padding={tablePadding}>Recursive function</Text></Appear>
+          <Appear><Text textSize={40} padding={tablePadding}>It cannot be optimized by V8</Text></Appear>
+          <Appear><Text textSize={40} padding={tablePadding}>Generic code</Text></Appear>
+          <Appear><Text textSize={40} padding={tablePadding}>Not type specific</Text></Appear>
+        </Slide>
+
+        <CodeSlide
+          transition={['slide']}
+          lang='js'
+          width={1000}
+          code={require('raw-loader!../assets/json.example')}
+          ranges={[
+            { loc: [0, 17], title: 'Example' },
+            { loc: [0, 1], note: 'Require the library' },
+            { loc: [3, 10], note: 'Schema definition' },
+            { loc: [3, 4], note: 'Declare the type' },
+            { loc: [4, 9], note: 'Object properties' },
+            { loc: [11, 16], note: 'Stringify the object' }
+          ]}
+        />
+
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Heading size={6} textColor='secondary'>{'Routing'}</Heading>
+          <Text textSize={40}><Link href='https://github.com/delvedor/find-my-way'>find-my-way</Link></Text>
+          <Image src={images.router} fit style={{ maxHeight: window.innerHeight - 300 }} />
+        </Slide>
+
+        <CodeSlide
+          transition={['slide']}
+          lang='js'
+          code={require('raw-loader!../assets/closures.example')}
+          ranges={[
+            { loc: [0, 10], title: 'The problem with closures' },
+            { loc: [2, 5], note: 'This function is short-lived and hard to optimize' },
+            { loc: [1, 6], note: 'bigdata is still in scope!' }
+          ]}
+        />
+
+        <CodeSlide
+          transition={['slide']}
+          lang='js'
+          code={require('raw-loader!../assets/no-closures.example')}
+          ranges={[
+            { loc: [0, 12], title: 'Avoid nested closures' },
+            { loc: [1, 4], note: 'bigdata exits scope here' },
+            { loc: [7, 10], note: 'this function can be optimized!' }
+          ]}
+        />
+
+        <Slide transition={['slide']} bgColor='primary'>
+          <Heading size={3} caps textColor='secondary'>{'Fastify has not closures at all!'}</Heading>
+          <Appear>
+            <Text margin={'40px 0 0 0'}>
+              <Link href='https://github.com/mcollina/reusify' target='_blank' textColor='secondary'>https://github.com/mcollina/reusify</Link>
+            </Text>
+          </Appear>
+          <Appear>
+            <Text margin={'15px 0 0 0'}>
+              <Link href='https://github.com/fastify/middie' target='_blank' textColor='secondary'>https://github.com/fastify/middie</Link>
+            </Text>
+          </Appear>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary'>
+          <Heading size={3} caps textColor='secondary'>{'Middlewares'}</Heading>
+          <Text margin={'40px 0 0 0'}>Fastify consumes Express middlewares</Text>
+          <Image src={images.helmet} />
+          <Text textSize={30}>Helmet comparison</Text>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary'>
+          <Heading size={3} caps textColor='secondary'>{'Validation'}</Heading>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary'>
+          <Heading size={3} caps textColor='secondary'>{'Architecture'}</Heading>
+          {/* plugins, encapsulation, separation of concerns, decorators, microservices */}
         </Slide>
 
         <Slide transition={['zoom']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
