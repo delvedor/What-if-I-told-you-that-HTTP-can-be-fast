5502f8bdb6872b687a45d148f48b98e77d19ade3
delvedor <tommydelved@gmail.com>
2017-09-13 09:35:51 +0200

    Updated slides

--- presentation/index.js
+++ presentation/index.js
@@ -62,7 +62,8 @@ const images = {
   dag: require('../assets/dag.png'),
   dagServices: require('../assets/dag-services.png'),
   tooling: require('../assets/tooling.jpg'),
-  architecture: require('../assets/architecture.jpg')
+  architecture: require('../assets/architecture.jpg'),
+  serialization: require('../assets/serialization.jpg')
 }
 
 preloader(images)
@@ -88,12 +89,6 @@ export default class Presentation extends React.Component {
           <Text size={6} margin={'150px 0 0 0'}>
             <Link href='https://github.com/delvedor' target='_blank' textColor='primary'>@delvedor</Link>
           </Text>
-        </Slide>
-
-        <Slide transition={['slide']} bgColor='primary'>
-          <Link href='http://www.letzdoitapp.com/' target='_blank' textColor='secondary'>
-            <Image src={images.letzdoit} width={600} />
-          </Link>
         </Slide>
 
         <CodeSlide
@@ -137,45 +132,6 @@ export default class Presentation extends React.Component {
         <Slide transition={['slide']} bgColor='primary'>
           <ImageSlide image={images.autocannonDemo} />
         </Slide>
-
-        {/* <Slide transition={['slide']} bgColor='tertiary'>
-          <Heading textColor='primary' size={3} margin={'0 0 50px 0'}>{'Run 0x'}</Heading>
-          <Terminal title='1. tmux -2 attach -t 0 (tmux)' output={[
-            '0x benchmarks/express.js',
-            <div>
-              <div>0x captures stacks using dtrace, which requires sudo access</div>
-              <div>Password: ••••••••</div>
-            </div>,
-            <div>
-              <div>Example app listening on port 3000!</div>
-              <div>Profiling</div>
-            </div>,
-            <div>
-              <div>Caught SIGINT, generating flamegraph</div>
-              <div>file:///Users/delvedor/Development/fastify/fastify/profile-68270/flamegraph.html</div>
-            </div>
-          ]}
-          />
-        </Slide>
-
-        <Slide transition={['slide']} bgColor='tertiary'>
-          <Heading textColor='primary' size={3} margin={'0 0 50px 0'}>{'Run autocannon'}</Heading>
-          <Terminal title='1. tmux -2 attach -t 0 (tmux)' output={[
-            'autocannon -c 100 -d 5 -p 10 localhost:3000',
-
-            <div><br />Running 5s test @ http://localhost:3000<br />
-            100 connections with 10 pipelining factor</div>,
-
-            <div>
-              <div style={{ color: '#94dadd' }}><br />Stat           Avg       Stdev       Max</div>
-              <div>Latency (ms)   8.36      25.17     118</div>
-              <div>Req/Sec        11733.6   341.12    12183</div>
-              <div>Bytes/Sec      2.69 MB   82.9 kB   2.88 MB</div>
-              <div><br />59k requests in 5s, 13.44 MB read</div>
-            </div>
-          ]}
-          />
-        </Slide> */}
 
         <Slide transition={['slide']} bgColor='quartenary'>
           <ImageSlide image={images.flamegraphExpress} />
@@ -257,9 +213,9 @@ export default class Presentation extends React.Component {
               </TableRow>
               <TableRow>
                 <TableItem textSize={40} padding={tablePadding}>req/sec</TableItem>
-                <TableItem textSize={40} textColor='red' padding={tablePadding}>8534</TableItem>
-                <TableItem textSize={40} textColor='red' padding={tablePadding}>2200</TableItem>
-                <TableItem textSize={40} textColor='green' padding={tablePadding}>21682</TableItem>
+                <TableItem textSize={40} textColor='red' padding={tablePadding}>18,740</TableItem>
+                <TableItem textSize={40} textColor='red' padding={tablePadding}>4,226</TableItem>
+                <TableItem textSize={40} textColor='green' padding={tablePadding}>29,340</TableItem>
               </TableRow>
             </TableBody>
           </Table>
@@ -273,13 +229,12 @@ export default class Presentation extends React.Component {
           <ImageSlide image={images.buildRun} />
         </Slide>
 
-        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
-          <Heading size={6} textColor='secondary'>{'Serialization'}</Heading>
-          <Text textSize={40}><Link href='https://github.com/fastify/fast-json-stringify'>fast-json-stringify</Link></Text>
-          <Image src={images.json} fit margin={'50px 0 0 0'} />
+        <Slide transition={['slide']} bgImage={images.serialization} bgDarken={0.6}>
+          <Heading size={3} caps textColor='primary'>{'Serialization'}</Heading>
         </Slide>
 
         <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <Text textSize={40} margin='0 0 50px 0'><Link href='https://github.com/fastify/fast-json-stringify'>fastify/fast-json-stringify</Link></Text>
           <Heading size={6} textColor='secondary'>{'Why not JSON.stringify?'}</Heading>
           <Appear><Text textSize={40} padding={tablePadding}>Recursive function</Text></Appear>
           <Appear><Text textSize={40} padding={tablePadding}>It cannot be optimized by V8</Text></Appear>
@@ -287,7 +242,11 @@ export default class Presentation extends React.Component {
           <Appear><Text textSize={40} padding={tablePadding}>Not type specific</Text></Appear>
         </Slide>
 
-        <CodeSlide
+        <Slide transition={['slide']} bgColor='primary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <ImageSlide image={images.json} />
+        </Slide>
+
+        {/* <CodeSlide
           transition={['slide']}
           lang='js'
           width={1000}
@@ -298,7 +257,7 @@ export default class Presentation extends React.Component {
             { loc: [2, 10], note: 'Schema definition' },
             { loc: [11, 16], note: 'Stringify the object' }
           ]}
-        />
+        /> */}
 
         <CodeSlide
           transition={['slide']}
@@ -433,6 +392,16 @@ export default class Presentation extends React.Component {
 
         <Slide transition={['slide']} bgColor='primary' align='flex-start center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
           <Text>DEMO!</Text>
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='tertiary' align='center center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
+          <ImageSlide image={images.fastifyWhiteVertical} />
+        </Slide>
+
+        <Slide transition={['slide']} bgColor='primary'>
+          <Link href='http://www.letzdoitapp.com/' target='_blank' textColor='secondary'>
+            <Image src={images.letzdoit} width={600} />
+          </Link>
         </Slide>
 
         <Slide transition={['slide']} bgColor='primary' align='flex-start center' maxHeight={window.innerHeight} maxWidth={window.innerWidth}>
